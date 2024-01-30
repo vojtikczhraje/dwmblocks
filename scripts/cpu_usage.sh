1c4c65370c7de93eb50da79a4c7be902526e2c48
vojtikczhraje <vojta271000@gmail.com>
2024-01-30 20:17:44 +0100

    Fixed cpu script

--- scripts/cpu_usage.sh
+++ scripts/cpu_usage.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
-echo "[  $cpu_usage ]"
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{printf "%.1f", 100 - $1}')
+echo "[  $cpu_usage% ]"
